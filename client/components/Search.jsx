8c3da8bad35b5aea9331072f80e0c2a9949c9e12
Hack Reactor Students <accounts@hackreactor.com>
2016-02-18 15:41:57 -0800

    Implement <Search /> with form event listeners

--- client/components/Search.jsx
+++ client/components/Search.jsx
@@ -1,1 +1,34 @@
-Search.jsx
+var Search = React.createClass({
+  getInitialState: function () {
+    return {
+      value: ''
+    };
+  },
+
+  // handles when text is typed into search box
+  handleInputChange: function (evt) {
+    this.setState({
+      value: evt.target.value
+    });
+  },
+
+  // handles when user hits 'Enter' key while in search box
+  handleSubmit: function (evt) {
+    evt.preventDefault();
+    this.props.searchMethod(this.state.value);
+  },
+
+  render: function () {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input
+          type="text" 
+          placeholder="Search..." 
+          onChange={function (evt) { 
+            this.handleInputChange(evt.target.value);
+          } 
+        }/>
+      </form>
+    );
+  }
+});
