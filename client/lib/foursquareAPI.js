1649806b9458e037e3c99904a6986387e3fe64b7
Hack Reactor Students <accounts@hackreactor.com>
2016-02-18 14:47:23 -0800

    Implement working API request for foursquare

--- client/lib/foursquareAPI.js
+++ client/lib/foursquareAPI.js
@@ -1,9 +1,11 @@
 var searchFoursquare = function(city, query, callback) {
 
-	$.get('https://api.foursquare.com/v2/venues/search?client_id=' + FOURSQUARE_CLIENT_ID + '&client_secret=' + FOURSQUARE_CLIENT_SECRET + '&near=' + city + '&query=' + query)
+	$.get('https://api.foursquare.com/v2/venues/search?client_id='+FOURSQUARE_CLIENT_ID+'&client_secret='+FOURSQUARE_CLIENT_SECRET+'&v=20130815&near='+city+'&query='+query)
 	.done(function(data) {
+		console.log("WORKS!");
 		callback(data);
 	}).fail(function(err) {
+		console.log('there was an error')
 		callback(err);
 	});
 
