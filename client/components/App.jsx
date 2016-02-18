f2f4a1fe132c3a1f8eb7d6b6518a7d3bd8c7a55f
Hack Reactor Students <accounts@hackreactor.com>
2016-02-18 11:22:45 -0800

    Implement App component, initialize with location state, mark up body, and render to DOM

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,1 +1,20 @@
-App.jsx
+var App = React.createClass({
+  getInitialState: function () {
+    return {
+      location: 'San Francisco' // initialize location state 
+    };
+  },
+
+  render: function () {
+    return (
+      <div className="container">
+        <Nav />
+        <main>
+          <Search location={this.state.location} />
+        </main>
+      </div>
+    );
+  }
+});
+
+ReactDOM.render(<App />, document.getElementById('app'));
