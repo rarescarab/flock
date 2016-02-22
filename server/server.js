264d1bc1337a70752de7d9e9f5b7d868cb548011
John Domingo <johnwill.domingo@gmail.com>
2016-02-22 13:36:53 -0800

    Reconfigure server to use webpack

--- server/server.js
+++ server/server.js
@@ -1,17 +1,23 @@
 var express = require('express');
 var mongoose = require('mongoose');
 
+var webpack = require('webpack');
+var WebpackDevServer = require('webpack-dev-server');
+var config = require('../webpack.config.js');
+
 var app = express();
-
 app.use(express.static('./client'));
+mongoose.connect('mongodb://localhost/flock');
 
-// mongoose.connect('mongodb://localhost/flock');
-
-// require('./config/middleware.js')(app, express);
-// require('./config/router.js')(app, express);
-
-app.listen(8000, function() {
-	console.log('listening on 8000');
+new WebpackDevServer(webpack(config), {
+  hot: true,
+  historyApiFallback: true,
+  proxy: {
+  	"*": "http://localhost:3000"
+  }
+}).listen(3000, 'localhost', function (err, result) {
+  if (err) {
+    console.log(err);
+  }
+  console.log('Flock 🐦  is listening on 3000');
 });
-
-module.exports = app;
