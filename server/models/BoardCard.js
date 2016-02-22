f70b9386d207fb309fdda37ec55fdfa14c9996fe
Elizabeth Mallalieu <lizmallalieu@gmail.com>
2016-02-22 14:09:14 -0800

    Update board card schema

--- server/models/BoardCard.js
+++ server/models/BoardCard.js
@@ -4,6 +4,7 @@ var boardCardSchema = new mongoose.Schema({
   title: String,
   description: String,
   venueId: Number,
+  createdAt: { type: Date, default: Date.now }
 });
 
 module.exports = mongoose.model('BoardCard', boardCardSchema);
