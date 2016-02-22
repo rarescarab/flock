86ab0c1db1f26948c30317c6f800c4d72a25d98b
John Domingo <johnwill.domingo@gmail.com>
2016-02-22 03:23:23 -0800

    Import Search component and return JSX in parentheses

--- client/components/Nav.jsx
+++ client/components/Nav.jsx
@@ -1,21 +1,25 @@
 var React = require('react');
 var ReactRouter = require('react-router');
 
+var Search = require('./Search.jsx');
+
 var Nav = React.createClass({
   renderAuthModal: function () {
     // display <AuthModal />
   },
 
   render: function () {
-    <nav>
-      <ul>
-        <li><a href="#">Home</a></li>
-        <li><Search searchMethod="this.props.explorePlace" /></li>
-        <li><Search searchMethod="this.props.searchPlace" /></li>
-        <li><a href="#">Log In</a></li>
-      </ul>
-    </nav>
-  };
+    return (
+      <nav>
+        <ul>
+          <li><a href="#">Home</a></li>
+          <li><Search searchMethod={this.props.explorePlace} /></li>
+          <li><Search searchMethod={this.props.searchPlace} /></li>
+          <li><a href="#">Log In</a></li>
+        </ul>
+      </nav>
+    );
+  }
 });
 
 module.exports = Nav;
