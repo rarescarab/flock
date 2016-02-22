d036ece4475179bcb8727a456ce71094f012181c
John Domingo <johnwill.domingo@gmail.com>
2016-02-22 15:16:35 -0800

    Reconfigure hot reload port and serve static index file on GET request

--- server/server.js
+++ server/server.js
@@ -1,23 +1,38 @@
 var express = require('express');
 var mongoose = require('mongoose');
+var path = require('path');
+var fs = require('fs');
 
 var webpack = require('webpack');
 var WebpackDevServer = require('webpack-dev-server');
 var config = require('../webpack.config.js');
 
 var app = express();
-app.use(express.static('./client'));
 mongoose.connect('mongodb://localhost/flock');
+app.use(express.static(path.resolve(__dirname, '../public')));
+
+app.get('/', function(request, response) {
+  response.writeHead(200, {'Content-Type': 'text/html'});
+	fs.readFile(path.resolve(__dirname, '../public/index.html'), function(err, data) {
+		if (err) {
+      console.error(err);
+    } else {
+		  response.send(data);
+    }
+	});
+});
+
+app.listen(3000);
 
 new WebpackDevServer(webpack(config), {
   hot: true,
   historyApiFallback: true,
   proxy: {
-  	"*": "http://localhost:3000"
+  	'*': 'http://localhost:3000'
   }
-}).listen(3000, 'localhost', function (err, result) {
+}).listen(3001, 'localhost', function (err, result) {
   if (err) {
     console.log(err);
   }
-  console.log('Flock 🐦  is listening on 3000');
+  console.log('Flock - Webpack 🐦  is listening on 3000 and 3001');
 });
