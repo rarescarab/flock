e20978e2ab93578ae5df9049de15d65df36ac5dc
John Domingo <johnwill.domingo@gmail.com>
2016-02-22 03:22:28 -0800

    Move form submit listener to opening tag and import Search component

--- client/components/Board/BoardCardModal.jsx
+++ client/components/Board/BoardCardModal.jsx
@@ -9,21 +9,21 @@ var BoardCardModal = React.createClass({
 	render: function(){
 		return (
 			<div>
-				//Collect information about new event
-				//Bind should give access of props to the parent for searching. 
-				<form class="newBoardCard">
+				{/* Collect information about new event
+					Bind should give access of props to the parent for searching */}
+				<form className="newBoardCard"  onClick={this.props.searchPlaces()}>
 
 					<input placeholder="Title" name="title" value={this.state.Title} onChange={this.handleInputChange}/>
 
 					<input placeholder="Description" name="description" value={this.state.description} onChange={this.handleInputChange}/>
 
-					//Get information from App Search places, and save on state/db 
+					{/* Get information from App Search places, and save on state/db */}
 
 					<input onSubmit={this.props.onSubmit.bind(null, this)} placeholder="Search"/>
 
-					//This button will change the state of submitted to be true. 
+					{/* This button will change the state of submitted to be true */}
 				<button> Enter Event </button>
-				</form onClick={this.props.searchPlaces()}>
+				</form>
 
 
 				<button onClick={this.handleClick}> Save Event </button>
