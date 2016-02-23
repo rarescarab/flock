57e10b5eb111d03d6aff27d61f7d6a93cd29a2b0
John Domingo <johnwill.domingo@gmail.com>
2016-02-22 19:25:39 -0800

    Orgranize server code, import mongoose models, and promisify mongoose methods

--- server/server.js
+++ server/server.js
@@ -1,29 +1,55 @@
 var express = require('express');
-var mongoose = require('mongoose');
 var path = require('path');
 var fs = require('fs');
+Q = require('q');
+var mongoose = require('mongoose');
 
 var webpack = require('webpack');
 var WebpackDevServer = require('webpack-dev-server');
 var config = require('../webpack.config.js');
 
-var app = express();
-mongoose.connect('mongodb://localhost/flock');
-app.use(express.static(path.resolve(__dirname, '../public')));
+/* -------------- */
+/*     MODELS     */
+/* -------------- */
 
-app.get('/', function(request, response) {
-  response.writeHead(200, {'Content-Type': 'text/html'});
-	fs.readFile(path.resolve(__dirname, '../public/index.html'), function(err, data) {
-		if (err) {
-      console.error(err);
-    } else {
-		  response.send(data);
-    }
-	});
+var User = require('./models/User');
+var Board = require('./models/Board');
+var Card = require('./models/BoardCard');
+
+/* ---------------- */
+/*     PROMISES     */
+/* ---------------- */
+
+var findUser = Q.nbind(User.findOne, User);
+var createUser = Q.nbind(User.create, User);
+var findBoard = Q.nbind(Board.findOne, Board);
+var createBoard = Q.nbind(Board.create, Board);
+var findCard = Q.nbind(Card.findOne, Card);
+var createCard = Q.nbind(Card.create, Card);
+
+/* ---------------- */
+/*     DATABASE     */
+/* ---------------- */
+
+mongoose.connect('mongodb://localhost/flock');
+var db = mongoose.connection;
+
+db.on('error', function() {
+	console.error.bind(console, 'Connection Error:');
 });
 
-app.listen(3000);
+db.once('open', function() {
+	console.log('MongoDB is open');
+});
 
+var app = express();
+app.use(express.static(path.resolve(__dirname, '../public')));
+
+/* --------------- */
+/*     SERVERS     */
+/* --------------- */
+
+app.listen(3000);
 new WebpackDevServer(webpack(config), {
   hot: true,
   historyApiFallback: true,
