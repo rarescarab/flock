a62494077802126ae88150a6e90fe27588b8883f
John Domingo <johnwill.domingo@gmail.com>
2016-02-23 11:19:49 -0800

    Refactor Nav component using Semantic UI and minor style tweak for search width

--- client/components/Nav.jsx
+++ client/components/Nav.jsx
@@ -11,19 +11,42 @@ var Nav = React.createClass({
   },
 
   render: function () {
+    var searchStyle = { width: 60 + '%'};
     return (
       <nav>
-        <ul>
-          <li><a href="#">Home</a></li>
-          <li><Search searchMethod={this.props.explorePlace} /></li>
-          <li><Search searchMethod={this.props.searchPlace} /></li>
-          <li>
-            <FacebookLogin
-                appId={FACEBOOK_APP_ID}
-                autoLoad={true}
-                callback={this.responseFacebook} />
-          </li>
-        </ul>
+        <div className="ui fixed inverted menu">
+          <div className="ui container">
+            <a href="#" className="header item">
+              <img className="logo" src="assets/images/logo.svg"></img>
+            </a>
+            <div className="item" style={searchStyle}>
+              <Search searchMethod={this.props.searchPlace} />
+            </div>
+            <div id="userMenu" className="ui simple right dropdown item">
+              Username<i className="dropdown icon"></i>
+              <div className="menu">
+                <a className="item" href="#"><i className="grid layout icon"></i>My Boards</a>
+                <div className="divider"></div>
+                <div className="item">
+                  <i className="user icon"></i>
+                    Account
+                  <i className="dropdown icon"></i>
+                  <div className="menu">
+                    <a className="item" href="#"><i className="wrench icon"></i>Settings</a>
+                    <a className="item" href="#"><i className="phone icon"></i>Support</a>
+                  </div>
+                </div>
+                <div className="divider"></div>
+                <a className="item" href="#">Sign Out</a>
+                <div className="divider"></div>
+                <FacebookLogin
+                    appId={FACEBOOK_APP_ID}
+                    autoLoad={true}
+                    callback={this.responseFacebook} />
+              </div>
+            </div>
+          </div>
+        </div>
       </nav>
     );
   }
