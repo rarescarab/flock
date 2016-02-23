a62f729262369daaca47f6ff8ff9b53efc487cd4
John Domingo <johnwill.domingo@gmail.com>
2016-02-22 17:44:34 -0800

    Modify default array type

--- server/models/User.js
+++ server/models/User.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 
 var userSchema = new mongoose.Schema({
   name: String,
-  itinerarys: [Number]
+  boards: []
 });
 
 module.exports = mongoose.model('User', userSchema);
