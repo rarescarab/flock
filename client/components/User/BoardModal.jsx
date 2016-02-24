216003a66f0e88b7d96aeba89126ba4df8a4b433
Andrew Bresee <andrew.p.bresee@gmail.com>
2016-02-24 15:32:19 -0800

    Update the board components and config.js

---
+++ client/components/User/BoardModal.jsx
@@ -0,0 +1,15 @@
+var React = require('react');
+var ReactRouter = require('react-router');
+
+var BoardModal = React.createClass({
+	//For adding new boards
+	renderBoardModal: function(){
+		return <BoardCardModal/>
+	},
+
+  render: function () {
+    return
+  };
+});
+
+module.exports = BoardModal;
