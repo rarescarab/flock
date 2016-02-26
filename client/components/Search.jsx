8aadd6b1254a3e7774199c97531a71642e1468a6
John Domingo <johnwill.domingo@gmail.com>
2016-02-25 16:55:39 -0800

    Add specificity to Semantic UI dropdown module selector
    Nested React components require specific jQuery and Semantic UI listeners

--- client/components/Search.jsx
+++ client/components/Search.jsx
@@ -9,7 +9,7 @@ var Search = React.createClass({
   },
 
   componentDidMount: function () {
-    $('.ui.dropdown').dropdown({
+    $('#zipcodeDropdown').dropdown({
       apiSettings: {
         headers: {'Access-Control-Allow-Origin': '*'},
         onResponse: function(response, settings) {
@@ -87,7 +87,7 @@ var Search = React.createClass({
           <i className="search icon"></i>
           <input onChange={this.handleInputChange} type="text" placeholder="Search"/>
         </form>
-        <div className="ui labeled icon top right pointing dropdown button">
+        <div id="zipcodeDropdown" className="ui labeled icon top right pointing dropdown button">
           <i className="marker icon"></i>
           <span id="locationFilter" className="text">San Francisco</span>
           <div className="menu">
