b304dd235b26a02ab8346b60a89a7364387ab9db
John Domingo <johnwill.domingo@gmail.com>
2016-02-25 16:39:04 -0800

    Implement searchVenue method and replace state with mock data

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -2,18 +2,17 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var ReactRouter = require('react-router');
 
-var configJSON = require('../../config.JSON');
-var configJS = require('../../config.js');
+var apiInfo = require('../../config');
+window.FOURSQUARE_CLIENT_ID = apiInfo.foursquare.client_ID;
+window.FOURSQUARE_CLIENT_SECRET = apiInfo.foursquare.client_secret;
+window.ZIPCODEAPI_KEY = apiInfo.zipcode.zipcodeapi_key;
 
-var apiInfo = require('../../config.js');
-var FOURSQUARE_CLIENT_ID = apiInfo.foursquare.client_ID
-var FOURSQUARE_CLIENT_SECRET = apiInfo.foursquare.client_secret
-
-console.log(configJSON);
+var mockState = require('./lists/mockState');
 
 // main components
 var Nav = require('./Nav.jsx');
 var Search = require('./Search.jsx');
+var Card = require('./Card.jsx');
 // board components
 var Board = require('./board/Board.jsx');
 var BoardCard = require('./board/BoardCard.jsx');
@@ -21,29 +20,16 @@ var BoardCardModal = require('./board/BoardCardModal.jsx');
 // feed components
 // var AuthModal = require('./feed/AuthModal.jsx');
 // var Feed = require('./feed/Feed.jsx');
-// var FeedCard = require('./feed/FeedCard.jsx');
+var FeedCard = require('./feed/FeedCard.jsx');
 // user components
-// var BoardModal = require('./user/BoardModal.jsx');
+var BoardModal = require('./user/BoardModal.jsx');
 // var User = require('./user/User.jsx');
 // var UserCard = require('./user/UserCard.jsx');
 
 
 var App = React.createClass({
   getInitialState: function () {
-    return {
-      user: {
-        _id: '1234567890',
-        username: 'John Doe',
-        boards: [],
-        home: 94608
-      },
-      location: {
-        city: 'Emeryville',
-        state: 'CA',
-        zip: 94608
-      },
-      locations: ['San Francisco, CA', 'New York, NY', 'Seattle, WA']
-    };
+    return mockState;
   },
 
   searchPlace: function (query, callback) {
@@ -66,6 +52,24 @@ var App = React.createClass({
     return;
   },
 
+  searchVenue: function (venueId, callback) {
+      console.log('Searching Foursquare for venue ID %s', venue);
+      var url = 'https://api.foursquare.com/v2/venues/' + venueId + '?client_id=' + FOURSQUARE_CLIENT_ID + '&client_secret=' + FOURSQUARE_CLIENT_SECRET + '&v=20160225';
+
+      $.get(url)
+      .done(function(data) {
+        console.log("GOT VENUE DATA!");
+        callback(data);
+        }).fail(function(err) {
+        console.log('there was an error')
+        callback(err);
+      });
+
+    //Query will come from BoardCardModal.
+    console.log('Searching Venue...');
+    return;
+  },
+
   explorePlace: function (query) {
     // Foursquare Explore API call to return inspiration data
     console.log('Exploring Place...');
@@ -73,6 +77,11 @@ var App = React.createClass({
   },
 
   render: function () {
+    var containerStyle = {
+      'marginTop': '50px',
+      'padding': '20px 30px'
+    };
+
     return (
       <div className="container">
         <Nav
@@ -81,10 +90,15 @@ var App = React.createClass({
           locations={this.state.locations}
         />
         <main>
-          {
-            // we think React Router goes in here somehow
-            // so that it renders Board
-          }
+          <div
+            className="ui link four fluid stackable special cards"
+            style={containerStyle}>
+            {
+              this.state.cards.map((card, key) =>
+                <Card data={card} key={key}/>
+              )
+            }
+          </div>
         </main>
       </div>
     );
