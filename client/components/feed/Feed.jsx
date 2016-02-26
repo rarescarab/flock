4bf3a489934576c28c0c7131b43ffea7947411d6
John Domingo <johnwill.domingo@gmail.com>
2016-02-26 11:24:22 -0800

    Render semantic-ui fluid stackable cards view

--- client/components/feed/Feed.jsx
+++ client/components/feed/Feed.jsx
@@ -1,10 +1,21 @@
 var React = require('react');
 var ReactRouter = require('react-router');
 
+var Card = require('../Card');
+var FeedCard = require('./FeedCard');
+
 var Feed = React.createClass({
   render: function () {
-    return;
-  };
+    return (
+      <main className="ui link four fluid stackable special cards" style={this.props.style}>
+        {this.props.cards.map((card, key) =>
+          <Card card={card} venues={this.props.venues} key={key}>
+            <FeedCard card={card} venues={this.props.venues} key={key}/>
+          </Card>
+        )}
+      </main>
+    )
+  }
 });
 
 module.exports = Feed;
