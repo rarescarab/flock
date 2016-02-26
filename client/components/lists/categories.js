5d05fd644bf0bba54cd8ed40afc79036a31bda80
John Domingo <johnwill.domingo@gmail.com>
2016-02-25 16:40:29 -0800

    Create lists to store mock application data such as users, categories, and states

---
+++ client/components/lists/categories.js
@@ -0,0 +1,9 @@
+module.exports = [
+	{name: 'Travel',			class: 'red world icon'},
+	{name: 'Weekend',			class: 'orange calendar icon'},
+	{name: 'Adventure',		class: 'green map icon'},
+	{name: 'Shopping',		class: 'blue shop icon'},
+	{name: 'Nightout',		class: 'teal birthday icon'},
+	{name: 'Birthday',		class: 'pink find icon'},
+	{name: 'Sightseeing',	class: 'violet cocktail icon'}
+];
---
+++ client/components/lists/mockState.js
@@ -0,0 +1,321 @@
+module.exports = {
+	user: {
+		_id: '56cdf15bef62061312bfc144',
+		username: 'bennyhung',
+		name: 'Benny Hung',
+		home: 94102,
+		friends: [
+			'56cdecf9c52f037b0e66709c', // @johndomingo
+			'56cdfa6d901de47718fba699',	// @liz
+			'56ce2974f2f1d75732dfb172'	// @andrew_bresee
+		]
+	},
+	location: {
+		city: 'San Francisco',
+		state: 'CA',
+		zip: 94102
+	},
+	locations: [
+		'San Francisco, CA',
+		'New York, NY',
+		'Seattle, WA',
+		'San Diego, CA',
+		'Oakland, CA',
+		'Kansas City, Missouri'
+	],
+	boards: [
+		'56cdff21330d76dd1d616c47',
+		'56ce0b812d9c207f28eebd10',
+		'56ce0bb1b1f4acb0283ff589',
+		'56ce0c18b82fed0529f26b06',
+		'56ce0c62b6bc984629387938',
+		'56ce0cfaa131e4be298cfa01',
+		'56ce0ef4ce84a3492b85c7fb',
+		'56ce0fc54c45e6172cf60dc4',
+		'56ce13e3484483a82f1e1a53',
+		'56ce251cf2f1d75732dfb171'
+	],
+	cards: [
+		{
+			id: "506ef69fe4b0eca4bc1d56f8",
+			name: "Ghirardelli Ice Cream & Chocolate Shop",
+			categories: [
+				{shortName: "Desserts", primary: true}
+			],
+			contact: {
+				formattedPhone: "(415) 536-7830",
+				facebook: "1389191947959755"
+			},
+			url: "http://ghirardelli.com",
+			location: {
+				address: "2 New Montgomery St",
+				city: "San Francisco",
+				state: "CA",
+				postalCode: "94105",
+				lat: 37.78878321579423,
+				lng: -122.4021411242791
+			},
+			stats: {
+				checkinsCount: 4656,
+				usersCount: 3643,
+				tipCount: 48,
+			},
+			hasMenu: true,
+			menu: {
+				mobileUrl: "https://foursquare.com/v/506ef69fe4b0eca4bc1d56f8/device_menu"
+			}
+		},
+		{
+			id: "4baae1f9f964a520f18b3ae3",
+			name: "Chile Pies & Ice Cream",
+			categories: [
+				{shortName: "Desserts", primary: true}
+			],
+			contact: {
+				formattedPhone: "(415) 614-9411",
+				facebook: "156884124354769"
+			},
+			url: "http://ghirardelli.com",
+			location: {
+				address: "601 Baker St",
+				city: "San Francisco",
+				state: "CA",
+				postalCode: "94117",
+				lat: 37.77652174066475,
+				lng: -122.44170159101488
+			},
+			stats: {
+				checkinsCount: 5437,
+				usersCount: 2872,
+				tipCount: 90,
+			},
+			hasMenu: true,
+			menu: {
+				externalUrl: "http://www.greenchilekitchen.com/chilepies-menu/"
+			}
+		},
+		{
+			id: "42b21280f964a5206e251fe3",
+			name: "Swensen's Ice Cream",
+			categories: [
+				{shortName: "Ice Cream", primary: true}
+			],
+			contact: {
+				formattedPhone: "(415) 775-6818",
+				facebook: "152678084749323"
+			},
+			url: "http://ghirardelli.com",
+			location: {
+				address: "1999 Hyde St",
+				city: "San Francisco",
+				state: "CA",
+				postalCode: "94109",
+				lat: 37.799060628977294,
+				lng: -122.41922467947006
+			},
+			stats: {
+				checkinsCount: 5737,
+				usersCount: 3755,
+				tipCount: 77
+			},
+			hasMenu: true,
+			menu: {
+				externalUrl: "http://swensens.com/menu/"
+			}
+		},
+		{
+			id: "4eac41a5dab40d132703fc44",
+			name: "The Ice Cream Bar Soda Fountain",
+			categories: [
+				{shortName: "Ice Cream", primary: true}
+			],
+			contact: {
+				formattedPhone: "(415) 742-4932",
+				facebook: "303090803075345"
+			},
+			url: "http://theicecreambarsf.com",
+			location: {
+				address: "815 Cole St",
+				city: "San Francisco",
+				state: "CA",
+				postalCode: "94117",
+				lat: 37.766434751363306,
+				lng: -122.45017118400813
+			},
+			stats: {
+				checkinsCount: 9599,
+				usersCount: 5603,
+				tipCount: 170
+			},
+			hasMenu: true,
+			menu: {
+				externalUrl: "http://swensens.com/menu/"
+			}
+		},
+		{
+			id: "4ae35463f964a520849321e3",
+			name: "Miyako Old Fashion Ice Cream Shop",
+			categories: [
+				{shortName: "Ice Cream", primary: true}
+			],
+			contact: {
+				formattedPhone: "(415) 931-5260"
+			},
+			url: "http://theicecreambarsf.com",
+			location: {
+				address: "1460 Fillmore St",
+				city: "San Francisco",
+				state: "CA",
+				postalCode: "94115",
+				lat: 37.783052326151484,
+				lng: -122.43286409378109
+			},
+			stats: {
+				checkinsCount: 507,
+				usersCount: 320,
+				tipCount: 10
+			}
+		},
+		{
+			id: "49e22ce6f964a520f9611fe3",
+			name: "Mitchell's Ice Cream",
+			categories: [
+				{shortName: "Desserts", primary: true}
+			],
+			contact: {
+				formattedPhone: "(415) 614-9411",
+				twitter: "mitchellsicecre",
+				facebook: "115681848447769"
+			},
+			url: "http://www.mitchellsicecream.com",
+			location: {
+				address: "688 San Jose Ave",
+				city: "San Francisco",
+				state: "CA",
+				postalCode: "94110",
+				lat: 37.74413536569657,
+				lng: -122.42281347513199
+			},
+			stats: {
+				checkinsCount: 15127,
+				usersCount: 8422,
+				tipCount: 229
+			},
+			hasMenu: true,
+			menu: {
+				mobileUrl: "https://foursquare.com/v/49e22ce6f964a520f9611fe3/device_menu"
+			}
+		}
+	],
+	venues: [
+		{	id: "506ef69fe4b0eca4bc1d56f8",
+			bestPhoto: {
+				suffix: "https://irs0.4sqi.net/img/general/300x300/3004096_2peBScwmAolATHRJlGTCAhEFDhzqbGT00ruO8PoFDxc.jpg"
+			},
+			rating: 8.1,
+			price: {
+				tier: 1,
+				message: "Cheap"
+			},
+			hours: {
+				timeframes: [
+					{ days: "Mon–Thu",
+						open: [ { renderedTime: "8:00 AM–10:00 PM" } ]
+					},
+					{ days: "Fri",
+						open: [ { renderedTime: "8:00 AM–11:00 PM" } ]
+					},
+					{ days: "Sat",
+						open: [ { renderedTime: "10:00 AM–11:00 PM" } ]
+					},
+					{ days: "Sun",
+						open: [ { renderedTime: "10:00 AM–10:00 PM" } ]
+					}
+				]
+			}
+		},
+		{	id: "4baae1f9f964a520f18b3ae3",
+			bestPhoto: {
+				suffix: "/12938919_3txDmYfgVTsaBKAajkOPL3bJ2bGBh865cdKkP3HPVgQ.jpg"
+			},
+			rating: 9.1,
+			price: {
+				tier: 2,
+				message: "Moderate"
+			},
+			hours: {
+				timeframes: [
+					{ days: "Mon–Sun",
+						open: [ { renderedTime: "Noon–10:00 PM" } ]
+					}
+				]
+			}
+		},
+		{ id: "4eac41a5dab40d132703fc44",
+			bestPhoto: {
+				suffix: "/IE5StngDjJBPW6w9MuiTg7WfWIOVhYxbbfcyXur23_w.jpg"
+			},
+			rating: 9.3,
+			price: {
+				tier: 1,
+				message: "Cheap"
+			},
+			hours: {
+				timeframes: [
+					{ days: "Mon–Sun",
+						open: [ { renderedTime: "Noon–10:00 PM" } ]
+					}
+				]
+			}
+		},
+		{ id: "42b21280f964a5206e251fe3",
+			bestPhoto: {
+				suffix: "/35139054_R5bV70OX7YLAzURky-usvOlnCb6Oxft-nQD675MYPlU.jpg"
+			},
+			rating: 9.1,
+			price: {
+				tier: 1,
+				message: "Cheap"
+			},
+			hours: {
+				timeframes: [
+					{ days: "Tue–Thu, Sun",
+						open: [ { renderedTime: "Noon–10:00 PM" } ]
+					},
+					{ days: "Fri–Sat",
+						open: [ { renderedTime: "Noon–11:00 PM" } ]
+					}
+				]
+			}
+		},
+		{	id: "4ae35463f964a520849321e3",
+			bestPhoto: {
+				suffix: "/186901_leZkhEb458uB-dCpwA6gWDqS7EAF8qqcinEig9eGwm8.jpg"
+			},
+			rating: 7.2,
+			price: {
+				tier: 1,
+				message: "Cheap"
+			},
+			hours: {
+				timeframes: [
+					{ days: "Fri",
+						open: [ { renderedTime: "2:00 PM–10:00 PM" } ]
+					},
+					{ days: "Sat",
+						open: [ { renderedTime: "1:00 PM–10:00 PM" } ]
+					},
+					{ days: "Sun",
+						open: [ { renderedTime: "Noon–10:00 PM" } ]
+					},
+					{ days: "Mon–Tue",
+						open: [ { renderedTime: "11:00 AM–9:00 PM" } ]
+					},
+					{ days: "Wed",
+						open: [ { renderedTime: "" } ]
+					}
+				]
+			}
+		}
+	]
+};
