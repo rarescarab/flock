6c0a5068d0878206367a3ff8cadbb1cdcb6c1503
John Domingo <johnwill.domingo@gmail.com>
2016-02-26 10:31:24 -0800

    Render Board cards, modal, and a parallax header.
    Define anticipated Board propTypes and default props. Implement methods for modal operations.

--- client/components/Board/Board.jsx
+++ client/components/Board/Board.jsx
@@ -1,40 +1,76 @@
 var React = require('react');
 var ReactRouter = require('react-router');
 
-
+var Card = require('../Card');
+var BoardModal = require('../user/BoardModal');
+var BoardCard = require('./BoardCard');
+var BoardCardModal = require('./BoardCardModal');
 
 var Board = React.createClass({
-	getInitialState: function(){
+	propTypes: {
+    title: React.PropTypes.string,
+    image: React.PropTypes.string,
+		description: React.PropTypes.string,
+		userId: React.PropTypes.string,
+		cards: React.PropTypes.array
+  },
+
+	getDefaultProps: function () {
 		return {
-			cardType: null,
-			currentBoard: null //All other info in database
+			title: 'Sample Board Title',
+			image: 'http://semantic-ui.com/images/wireframe/image.png',
+			description: 'Lorem ipsum...Some dummy board data goes here.',
+			userId: '',
+			cards: []
 		}
 	},
 
-	renderBoardCardModal: function(){
-		return <BoardCardModal/>
+	revealModal: function(){
+		$('#boardModal.ui.modal').modal('show');
 	},
 
-	handleClick: function(){
-		this.renderBoardCardModal()
+	resizeModal: function(){
+		$('#boardModal.ui.modal').modal('refresh');
 	},
 
-	render: function(){
-		var boardCards = this.props.cards.map(function(card){
-			return <BoardCard cardDetails={this.props.cards.card}/>
-		});
+	render: function () {
+		var headStyle = {
+			'marginTop': '50px',
+			'height': '60vh',
+			'backgroundImage': `url("${this.props.board.image}")`,
+			'backgroundAttachment': 'fixed',
+			'backgroundSize': 'cover',
+			'backgroundPosition': 'center 65%',
+			'backgroundRepeat': 'no-repeat'
+		};
 
 		return (
-			<div>
-				<h3>{this.title}</h3>
-				{boardCards}
-				<button onClick={this.handleClick}> Add New Event </button>
-			</div>
+			<section>
+				<header style={headStyle}>
+					<div className="ui compact segment">
+						<h1>{this.props.board.title}</h1>
+					</div>
+				</header>
+
+				<main style={this.props.style}>
+					<div className='ui fluid'>
+						{this.props.board.cards.map((card, key) =>
+							<BoardCard board={this.props.board} card={card} venues={this.props.venues} key={key}>
+							</BoardCard>
+						)}
+					</div>
+
+					<div className="ui center aligned basic segment">
+						<div onClick={this.revealModal} className="ui center massive circular icon blue button">
+							<i className="plus icon"></i>
+						</div>
+					</div>
+				</main>
+
+				<BoardModal/>
+			</section>
 		)
-
-
 	}
-
 });
 
 module.exports = Board;
