b918fc8067cf93fcfc9803587e82da9f0950c84a
John Domingo <johnwill.domingo@gmail.com>
2016-02-26 15:43:34 -0800

    Define server-side API routing endpoints

--- server/config/router.js
+++ server/config/router.js
@@ -1,3 +1,28 @@
-module.exports = function(app, express) {
-	
+var User = require('../constrollers/userController');
+var Board = require('../controllers/boardController');
+var Card = require('../controllers/cardController');
+var Venue = require('../controllers/venueController');
+
+module.exports = function (app, express) {
+  app.get('/api/users', User.fetchOne);
+  app.post('/api/users', User.createOne);
+  app.put('/api/users', User.updateOne);
+  app.delete('/api/users', User.deleteOne);
+  app.get('/api/users/boards', User.populateList);
+
+  app.get('/api/boards', Board.fetchOne);
+  app.post('/api/boards', Board.createOne);
+  app.put('/api/boards', Board.updateOne);
+  app.delete('/api/boards', Board.deleteOne);
+  app.get('/api/boards/cards', Board.populateList);
+
+  app.get('/api/cards', Card.fetchOne);
+  app.post('/api/cards', Card.createOne);
+  app.put('/api/cards', Card.updateOne);
+  app.delete('/api/cards', Card.deleteOne);
+
+  app.get('/api/venues', Venue.fetchOne);
+  app.post('/api/venues', Venue.createOne);
+  app.put('/api/venues', Venue.updateOne);
+  app.delete('/api/venues', Venue.deleteOne);
 };
