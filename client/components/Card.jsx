65955d68396005cc4ec32103be76e6a626a68fca
John Domingo <johnwill.domingo@gmail.com>
2016-02-26 10:53:59 -0800

    Provide fallback for missing categories and images
    Add semantic-ui dimmer and import categories list.

--- client/components/Card.jsx
+++ client/components/Card.jsx
@@ -1,9 +1,12 @@
 var React = require('react');
 var ReactRouter = require('react-router');
 
+var list = require('./lists/categories');
+
 var Card = React.createClass({
   componentDidMount: function () {
     $('.ui.bottom.attached.progress').progress();
+    $('.special.cards .image').dimmer({on: 'hover'});
   },
 
   render: function () {
@@ -17,12 +20,16 @@ var Card = React.createClass({
       'marginTop': '-2px'
     };
 
+    var category = this.props.card.category || this.props.card.categories[0].shortName;
+
+    // create a simple method here that pushes each unique category as a key in an object and assign it a color from a set array of colors
+
     return (
       <div className="card">
         <div className="content">
-          <div className="ui orange left ribbon label">
-            <i className="spoon icon"></i>
-            {this.props.data.categories[0].shortName}
+          <div className={`ui ${list[category] ? list[category].color : 'black'} left ribbon label`}>
+            <i className={`${list[category] ? list[category].icon : ''} icon`}></i>
+            {category}
           </div>
         </div>
         <div className="blurring dimmable image">
@@ -33,10 +40,10 @@ var Card = React.createClass({
               </div>
             </div>
           </div>
-          <img className="ui image" src="http://semantic-ui.com/images/avatar/large/elliot.jpg"/>
+          <img className="ui image" src={this.props.card.image || `https://irs0.4sqi.net/img/general/300x300${this.props.venues[this.props.card.id].bestPhoto.suffix}`}/>
         </div>
         {this.props.children}
-        <div className="extra content">
+        <div className="ui extra content bottom attached segment">
           <div className="ui large transparent left icon input">
             <i className="comment icon"></i>
             <input type="text" placeholder="Add Comment..."/>
