9e03ba6ea14bc0018657922627dee16096a554d8
Andrew Bresee <andrew.p.bresee@gmail.com>
2016-02-27 15:09:43 -0800

    Write three functioning tests

--- test/test.js
+++ test/test.js
@@ -8,4 +8,23 @@ describe('server', function() {
       done();
     });
   });
+
+  it('should respond to GET requests for the users page with a 200 status code', function(done) {
+    request('http://localhost:3001/#/api/users', function(error, response, body) {
+      console.log("The response is : ", response.body)
+      expect(response.statusCode).to.equal(200);
+      done();
+    });
+  });
+
+  it('should send back an object', function(done) {
+    request('http://localhost:3001/#/api/users', function(error, response, body) {
+      // parsedBody = JSON.parse(response);
+      console.log("RESPONSE.BODY : ", response.body)
+      expect(response).to.be.an('object');
+      done();
+    });
+  });
+
+
 });
