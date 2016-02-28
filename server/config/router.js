d403f594721f3304c24750451f8cfd2e98d462ff
John Domingo <johnwill.domingo@gmail.com>
2016-02-27 22:38:22 -0800

    Remove named parameters from GET endpoints

--- server/config/router.js
+++ server/config/router.js
@@ -6,22 +6,22 @@ var Card = require('../controllers/cardController');
 var Venue = require('../controllers/venueController');
 
 module.exports = function (app, express) {
-  app.get('/api/users/:id', User.fetchOne);
+  app.get('/api/users', User.fetchOne);
   app.post('/api/users', User.createOne);
   app.put('/api/users', User.updateOne);
   app.delete('/api/users', User.removeOne);
 
-  app.get('/api/boards/:id', Board.fetchOne);
+  app.get('/api/boards', Board.fetchOne);
   app.post('/api/boards', Board.createOne);
   app.put('/api/boards', Board.updateOne);
   app.delete('/api/boards', Board.removeOne);
 
-  app.get('/api/cards/:id', Card.fetchOne);
+  app.get('/api/cards', Card.fetchOne);
   app.post('/api/cards', Card.createOne);
   app.put('/api/cards', Card.updateOne);
   app.delete('/api/cards', Card.removeOne);
 
-  app.get('/api/venues/:id', Venue.fetchOne);
+  app.get('/api/venues', Venue.fetchOne);
   app.post('/api/venues', Venue.createOne);
   app.put('/api/venues', Venue.updateOne);
   app.delete('/api/venues', Venue.removeOne);
