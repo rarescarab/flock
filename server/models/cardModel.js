8ee4396bb6efbfb5929ff0638257efdc6964ca25
John Domingo <johnwill.domingo@gmail.com>
2016-02-27 22:22:42 -0800

    Add venues array to store Venue IDs

--- server/models/cardModel.js
+++ server/models/cardModel.js
@@ -1,11 +1,15 @@
 var mongoose = require('mongoose');
+var ObjectId = mongoose.Schema.Types.ObjectId;
+
+var Venue = require('./venueModel');
 
 var cardSchema = new mongoose.Schema({
-  userTitle: String,
+  title: String,
   description: String,
   venueId: Number,
   createdAt: Date,
-  startTime: Date
+  startTime: Date,
+  venues: [{type: ObjectId, ref: Venue}]
 });
 
 module.exports = mongoose.model('Card', cardSchema);
