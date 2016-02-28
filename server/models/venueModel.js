459ed022839a0e8491459b745efeafc9eb4f8b6e
John Domingo <johnwill.domingo@gmail.com>
2016-02-27 22:24:08 -0800

    Change photoSuffix field to photo

--- server/models/venueModel.js
+++ server/models/venueModel.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 
 var venueSchema = new mongoose.Schema({
   venueId: String,
-  photoSuffix: String,
+  photo: String,
   rating: Number,
   hours: [{
     days: String,
