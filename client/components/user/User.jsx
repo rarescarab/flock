374e7d07965ae5eb5607a45513157a9044a055bb
John Domingo <johnwill.domingo@gmail.com>
2016-02-29 00:49:44 -0800

    Add logic for generating permalinks + ES6 refactor

--- client/components/user/User.jsx
+++ client/components/user/User.jsx
@@ -1,26 +1,96 @@
-var React = require('react');
-var ReactRouter = require('react-router');
+import React from 'react'
 
-var Card = require('../Card');
-var UserCard = require('./UserCard');
-var Modal = require('../Modal');
-var UserModal = require('../user/UserModal');
+import Card from '../Card'
+import UserCard from './UserCard'
+import Modal from '../Modal'
+import UserModal from './UserModal'
 
-var User = React.createClass({
-  propTypes: {
-    user: React.PropTypes.object.isRequired,
-    boards: React.PropTypes.array.isRequired
-  },
+/* -------------- */
+/*     User     */
+/* -------------- */
 
-  revealModal: function(){
-    $('#boardModal.ui.modal').modal('show');
-  },
+class User extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      permalink: '',
+      valid: true,
+      pristine: true,
+      mode: 'userModal',
+      dimmer: ''
+    }
+  }
 
-  resizeModal: function(){
-    $('#boardModal.ui.modal').modal('refresh');
-  },
+  generateLink = (str) => {
+    return str.replace(/[^a-z0-9]+/gi, '-')
+    .replace(/^-*|-*$/g, '').toLowerCase()
+  }
 
-  render: function () {
+  suggestLink = (evt) => {
+    if (this.state.pristine) {
+      let link = this.generateLink(evt.target.value)
+      this.setState({permalink: link})
+    }
+  }
+
+  changeLink = (evt) => {
+    this.generatePermalink(evt.target.value)
+    this.setState({pristine: false})
+  }
+
+  checkLink = () => {
+    console.log('CHECKING API FOR LINK NOW',this)
+    $.get('/api/boards', {
+      permalink: this.state.permalink,
+      username: this.props.user.username
+    })
+    .done(function(data) {
+      if (data) {
+        console.log('GOT BOARD DATA BACK!',this)
+        this.setState({valid: true})
+      } else {
+        console.log('GOT NO BOARD DATA BACK!',this)
+        this.setState({valid: false})
+      }
+    }).fail(function(err) {
+      console.log('GOT AN ERROR FROM API!',this)
+      this.setState({valid: false})
+      console.error('Could not get board data from server', err)
+      throw new Error('Could not get board data from server', err)
+    })
+  }
+
+  toggleModal = () => {
+    if (this.state.dimmer) {
+      this.setState({dimmer: ''})
+    } else {
+      this.setState({dimmer: 'active visible'})
+    }
+  }
+
+  showModal = (evt) => {
+    var $modal = $('#' + this.state.mode + 'Modal')
+    var $form = $('#' + this.state.mode + 'Form')
+    var $title = $('#' + this.state.mode + 'Title')
+    var $desc = $('#' + this.state.mode + 'Description')
+    var $pvt = $('#' + this.state.mode + 'Secret')
+    var $flock = $('#' + this.state.mode + 'Flock')
+
+    $modal.modal({
+      observeChanges: true,
+      onDeny: function (evt) {},
+      onApprove: function (evt) {
+        var title = $title.val()
+        var description = $desc.val()
+        var isPrivate = $pvt.val()
+        var flock = $flock.val()
+        $form.form('get value', 'userModalTitle')
+      }
+    })
+    .modal('toggle')
+  }
+
+  render () {
     var centerStyle = {
       'display': '-webkit-flex',
       'display': 'flex',
@@ -33,11 +103,9 @@ var User = React.createClass({
       'borderBottom': '2px solid #2185d0',
       'borderTop': 'none',
       'marginBottom': '20px'
-    };
+    }
 
-    var statStyle = {
-      'marginBottom': 0
-    };
+    var statStyle = { 'marginBottom': 0 }
 
     return (
       <section style={this.props.style}>
@@ -75,17 +143,35 @@ var User = React.createClass({
             )}
           </div>
           <div className="ui center aligned basic segment">
-            <div onClick={this.revealModal} className="ui center massive circular icon blue button">
+            <div onClick={this.toggleModal.bind(this)} className="ui center massive circular icon pink button">
               <i className="plus icon"></i>
             </div>
           </div>
         </main>
-        <Modal>
-          <UserModal/>
+        <Modal
+          mode={this.state.mode}
+          dimmer={this.state.dimmer}
+          user={this.props.user}
+          header={'Create a board'}
+          suggestLink={this.suggestLink}
+          checkLink={this.checkLink}
+        >
+          <UserModal
+            user={this.props.user}
+            mode={this.state.mode}
+            permalink={this.state.permalink}
+            changeLink={this.changeLink}
+            checkLink={this.checkLink}
+          />
         </Modal>
       </section>
     )
   }
-});
+}
 
-module.exports = User;
+User.propTypes = {
+  user: React.PropTypes.object.isRequired,
+  boards: React.PropTypes.array.isRequired
+}
+
+module.exports = User
